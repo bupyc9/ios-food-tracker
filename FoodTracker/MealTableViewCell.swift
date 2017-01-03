5f7bfc27c796fd0a8d1f57cc96e0a73b75afe1bc
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-01-03 14:54:28 +0300

    Add class MealTableViewCell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Павел Афанасьев on 03/01/2017.
+//
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
