e14f8027049cd28332f57949e07ba7a8fde85a8e
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-01-04 23:45:17 +0300

    Load data and view on table view controller

---
+++ FoodTracker/MealTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Павел Афанасьев on 03/01/2017.
+//
+//
+
+import UIKit
+
+class MealTableViewController: UITableViewController {
+    //MARK: Properties
+    
+    var meals = [Meal]()
+    
+    //MARK: Private Methods
+    
+    private func loadSampleMeals() {
+        
+        let photo1 = UIImage(named: "meal1")
+        let photo2 = UIImage(named: "meal2")
+        let photo3 = UIImage(named: "meal3")
+        
+        guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
+            fatalError("Unable to instantiate meal1")
+        }
+        
+        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+            fatalError("Unable to instantiate meal2")
+        }
+        
+        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
+            fatalError("Unable to instantiate meal2")
+        }
+        
+        meals += [meal1, meal2, meal3]
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadSampleMeals()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        
+        // Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+        
+        return cell
+    }
+}
